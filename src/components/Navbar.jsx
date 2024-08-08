e57db925a02e7f14c7fe5ebffef8c7493fa02163
unknown <malik.rizwan177@yahoo.com>
2024-08-08 19:16:36 +0500

    mobilemenu navbar font color corrected

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,7 +50,7 @@ const Navbar = () => {
                     onClick={() => setMobileMenuOpen(false)}
                     key={item.id}
                     href={item.link}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50 hover:text-black"
                   >
                     {item.title}
                   </a>
