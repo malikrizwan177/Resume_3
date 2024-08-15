7a0fb950d2dd3b8dbae9b3466624dde3b707358a
unknown <malik.rizwan177@yahoo.com>
2024-08-15 14:52:24 +0500

    lazyloading added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Experience from "./components/Experience";
-import Hero from "./components/Hero";
+import { lazy, Suspense } from "react";
+
 import Navbar from "./components/Navbar";
-import Project from "./components/Project";
-import Skills from "./components/Skills";
+const Hero = lazy(() => import("./components/Hero"))
+const About = lazy(() => import("./components/About"))
+const Project = lazy(() => import("./components/Project"))
+const Skills = lazy(() => import("./components/Skills"))
+const Experience = lazy(() => import("./components/Experience"))
+const Contact = lazy(() => import("./components/Contact"))
 
 const App = () => {
   return (
     <div className="m-8 md:m-9 lg:m-10 py-9 lg:py-14 px-10 lg:px-32 rounded-3xl shadow-inner mbg h-full mborder">
       <Navbar/>
-      <Hero/>
-      <About/>
-      <Project/>
-      <Skills/>
-      <Experience/>
-      <Contact/>
+      <Suspense>
+        <Hero/>
+        <About/>
+        <Project/>
+        <Skills/>
+        <Experience/>
+        <Contact/>
+      </Suspense>
     </div>
   );
 };
